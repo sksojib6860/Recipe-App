9602b8c3e24c88cbdaf523504383ce2e69c646e2
SK Sojib <sksojib6860@gmail.com>
2022-02-12 20:15:21 +0600

    Recipe App Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:nasim_sir_project/model.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 main() {
   runApp(MaterialApp(
@@ -21,30 +22,32 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   List<Model> list = <Model>[];
+  String? text;
   final url =
       'https://api.edamam.com/search?q=chicken&app_id=4719113b&app_key=098f9b18cf3e480f5fd6ff3290df7fc7';
 
   getApiData() async {
     var response = await http.get(Uri.parse(url));
     Map json = jsonDecode(response.body);
-    json['hits'].forEach((e){
+    json['hits'].forEach((e) {
       Model model = Model(
-          url: e['recipe']['uri'],
+          url: e['recipe']['url'],
           image: e['recipe']['image'],
           source: e['recipe']['source'],
-          label: e['recipe']['label']
-      );
+          label: e['recipe']['label']);
       setState(() {
         list.add(model);
       });
     });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getApiData();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +62,20 @@ class _MyAppState extends State<MyApp> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               TextField(
+                onChanged: (v) {
+                  text = v;
+                },
                 decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SearchPage(search: text,)));
+                      },
+                      icon: Icon(Icons.search),
+                    ),
+                    hintText: 'Search for Recipe',
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20)),
                     fillColor: Colors.green.withOpacity(0.04),
@@ -75,46 +91,46 @@ class _MyAppState extends State<MyApp> {
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
                       crossAxisSpacing: 15,
-                      mainAxisSpacing: 15
-                  ),
+                      mainAxisSpacing: 15),
                   itemCount: list.length,
-                  itemBuilder: (context,i){
+                  itemBuilder: (context, i) {
                     final x = list[i];
                     return InkWell(
-                      onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>WebPage(
-                          url: x.url,
-                        )));
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => WebPage(
+                                      url: x.url,
+                                    )));
                       },
                       child: Container(
                         decoration: BoxDecoration(
                             image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: NetworkImage(x.image.toString()),
-                            )
-                        ),
-
+                          fit: BoxFit.fill,
+                          image: NetworkImage(x.image.toString()),
+                        )),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Container(
                               padding: EdgeInsets.all(10),
-                              height:40,
+                              height: 40,
                               color: Colors.black.withOpacity(0.5),
                               child: Center(child: Text(x.label.toString())),
                             ),
                             Container(
                               padding: EdgeInsets.all(10),
-                              height:40,
+                              height: 40,
                               color: Colors.black.withOpacity(0.5),
-                              child: Center(child: Text("Source : " + x.source.toString())),
+                              child: Center(
+                                  child:
+                                      Text("Source : " + x.source.toString())),
                             ),
                           ],
                         ),
                       ),
                     );
-
-
                   })
             ],
           ),
@@ -124,9 +140,7 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
 class WebPage extends StatelessWidget {
-
   final url;
   WebPage({this.url});
 
@@ -136,6 +150,111 @@ class WebPage extends StatelessWidget {
       body: SafeArea(
         child: WebView(
           initialUrl: url,
+        ),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  String? search;
+  SearchPage({this.search});
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<Model> list = <Model>[];
+  String? text;
+
+  getApiData(search) async {
+    final url =
+        'https://api.edamam.com/search?q=$search&app_id=4719113b&app_key=098f9b18cf3e480f5fd6ff3290df7fc7';
+
+    var response = await http.get(Uri.parse(url));
+    Map json = jsonDecode(response.body);
+    json['hits'].forEach((e) {
+      Model model = Model(
+          url: e['recipe']['url'],
+          image: e['recipe']['image'],
+          source: e['recipe']['source'],
+          label: e['recipe']['label']);
+      setState(() {
+        list.add(model);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getApiData(widget.search);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipe'),
+        elevation: 0,
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              GridView.builder(
+                  physics: const ScrollPhysics(),
+                  shrinkWrap: true,
+                  primary: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 15,
+                      mainAxisSpacing: 15),
+                  itemCount: list.length,
+                  itemBuilder: (context, i) {
+                    final x = list[i];
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => WebPage(
+                                      url: x.url,
+                                    )));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(x.image.toString()),
+                        )),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              height: 40,
+                              color: Colors.black.withOpacity(0.5),
+                              child: Center(child: Text(x.label.toString())),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              height: 40,
+                              color: Colors.black.withOpacity(0.5),
+                              child: Center(
+                                  child:
+                                      Text("Source : " + x.source.toString())),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  })
+            ],
+          ),
         ),
       ),
     );
